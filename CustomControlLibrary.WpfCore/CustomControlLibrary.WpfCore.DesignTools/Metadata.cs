b5f9d5d995cd7fbcf4c1319ef04db49241b35ed9
Hemant Bajpai <hebajpai@microsoft.com>
2019-07-05 14:48:39 -0700

    Added extensibility sample project

---
+++ CustomControlLibrary.WpfCore/CustomControlLibrary.WpfCore.DesignTools/Metadata.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.DesignTools.Extensibility.Metadata;
+using Microsoft.VisualStudio.DesignTools.Extensibility.Features;
+using Microsoft.VisualStudio.DesignTools.Extensibility.PropertyEditing;
+
+// The ProvideMetadata assembly-level attribute indicates to designers
+// that this assembly contains a class that provides an attribute table. 
+[assembly: ProvideMetadata(typeof(CustomControlLibrary.WpfCore.DesignTools.Metadata))]
+namespace CustomControlLibrary.WpfCore.DesignTools
+{
+    // Container for any general design-time metadata to initialize.
+    // Designers look for a type in the design-time assembly that 
+    // implements IProvideAttributeTable. If found, designers instantiate 
+    // this class and access its AttributeTable property automatically.
+    internal class Metadata : IProvideAttributeTable
+    {
+        // Accessed by the designer to register any design-time metadata.
+        public AttributeTable AttributeTable
+        {
+            get
+            {
+                AttributeTableBuilder builder = new AttributeTableBuilder();
+
+                // Add the providers to the design-time metadata.
+                builder.AddCustomAttributes(
+                    "CustomControlLibrary.WpfCore.CustomButton",
+                    new FeatureAttribute(typeof(CustomButtonDefaultInitializer)));
+                builder.AddCustomAttributes(
+                    "CustomControlLibrary.WpfCore.CustomButton",
+                    new FeatureAttribute(typeof(OpacitySliderAdornerProvider)));
+                builder.AddCustomAttributes(
+                    "CustomControlLibrary.WpfCore.CustomButton",
+                    new FeatureAttribute(typeof(CustomContextMenuProvider)));
+                builder.AddCustomAttributes(
+                    "CustomControlLibrary.WpfCore.CustomButton",
+                    new FeatureAttribute(typeof(CustomButtonParentAdapter)));
+                builder.AddCustomAttributes(
+                    "CustomControlLibrary.WpfCore.CustomButton",
+                    new FeatureAttribute(typeof(CustomButtonPlacementAdapter)));
+
+
+                // Add the propertyvalueeditors to the design-time metadata.
+                builder.AddCustomAttributes(
+                    "CustomControlLibrary.WpfCore.CustomButton", "Date",
+                    PropertyValueEditor.CreateEditorAttribute(
+                    typeof(DateInlinePropertyValueEditor)));
+                builder.AddCustomAttributes(
+                    "CustomControlLibrary.WpfCore.CustomButton", "Background",
+                    PropertyValueEditor.CreateEditorAttribute(
+                    typeof(BrushExtendedPropertyValueEditor)));
+
+                return builder.CreateTable();
+            }
+        }
+    }
+}
---
+++ CustomControlLibrary.WpfCore/CustomControlLibrary.WpfCore/CustomButton.cs
@@ -0,0 +1,46 @@
+﻿using System.Windows.Controls;
+using System.ComponentModel;
+using System;
+using System.Windows;
+
+namespace CustomControlLibrary.WpfCore
+{
+    public class CustomButton : Button
+    {
+        public CustomButton()
+        {
+            // The GetIsInDesignMode check and the following design-time 
+            // code are optional and shown only for demonstration.
+            if (DesignerProperties.GetIsInDesignMode(this))
+            {
+                Content = "Design mode active";
+            }
+        }
+
+        // Adding a Date property to CustomButton to provide sample code for PropertyValueEditor
+        public static readonly DependencyProperty DateProperty = DependencyProperty.Register(
+            "Date",
+            typeof(DateTime),
+            typeof(CustomButton),
+            new PropertyMetadata(DateTime.MinValue, OnDateChanged));
+
+        public DateTime Date
+        {
+            get
+            {
+                return (DateTime)this.GetValue(DateProperty);
+            }
+
+            set
+            {
+                this.SetValue(DateProperty, value);
+            }
+        }
+
+        private static void OnDateChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            CustomButton presenter = d as CustomButton;
+            presenter.Date = (DateTime)e.NewValue;
+        }
+    }
+}
